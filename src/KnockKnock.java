de2c7ea73919b3beab63b55c50d285456c0c6ebe
J.D.S. Solutions <league@iMac-2.attlocal.net>
2018-03-04 15:06:25 -0800

    Knock Knock/Robot Square/Last Summer

---
+++ src/KnockKnock.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"Knock Knock");
+JOptionPane.showMessageDialog(null,"Who's there?");
+JOptionPane.showMessageDialog(null,"Beats");
+JOptionPane.showMessageDialog(null,"Beats who?");
+JOptionPane.showMessageDialog(null,"Beats me");
+}
+}
--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -11,16 +11,19 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot cool = new Robot();
 
         // 3. Put the robot's pen down
-
+cool.penDown();
 
         // 6. Make the robot move as fast as possible
-
+cool.setSpeed(999999);
 
         // 5. Do everything below here 4 times
-
+for(int i = 0; i<4; i++) {
+	cool.move(200);
+	cool.turn(90);
+}
 
         //         2. Move your robot 200 pixels
 
